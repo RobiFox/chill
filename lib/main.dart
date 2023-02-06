beb666d1b66a0ccb45e0345a3e5ab9742a634dae
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-06 15:07:30 +0100

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,13 +35,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _urlController = TextEditingController();
+  final _responseController = TextEditingController();
+
+  Future<Response>? _future;
+
   bool isPhone() {
-    return Theme.of(context).platform == TargetPlatform.android || Theme.of(context).platform == TargetPlatform.iOS;
+    return Theme.of(context).platform == TargetPlatform.android ||
+        Theme.of(context).platform == TargetPlatform.iOS;
   }
 
   @override
   Widget build(BuildContext context) {
     double buttonSize = isPhone() ? 64 : 48;
+    String displayContent;
 
     return Scaffold(
       /*appBar: AppBar(
@@ -48,10 +57,14 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: [
-            if(isPhone()) SizedBox(height: 32,),
+            if (isPhone())
+              SizedBox(
+                height: 32,
+              ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
+                controller: _urlController,
                 keyboardType: TextInputType.url,
                 autocorrect: false,
                 decoration: InputDecoration(
@@ -62,7 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(128),
-                        borderSide: const BorderSide(color: Colors.grey, width: 2))),
+                        borderSide:
+                            const BorderSide(color: Colors.grey, width: 2))),
               ),
             ),
             Padding(
@@ -72,45 +86,127 @@ class _MyHomePageState extends State<MyHomePage> {
                 spacing: 8,
                 runSpacing: isPhone() ? -8 : 8,
                 children: [
-                  SizedBox(height: buttonSize, child: FittedBox(child: ElevatedButton(onPressed: () {}, child: const Text("POST"), ))),
-                  SizedBox(height: buttonSize, child: FittedBox(child: ElevatedButton(onPressed: () {}, child: const Text("GET"), ))),
-                  SizedBox(height: buttonSize, child: FittedBox(child: ElevatedButton(onPressed: () {}, child: const Text("PUT"), ))),
-                  SizedBox(height: buttonSize, child: FittedBox(child: ElevatedButton(onPressed: () {}, child: const Text("PATCH"), ))),
-                  SizedBox(height: buttonSize, child: FittedBox(child: ElevatedButton(onPressed: () {}, child: const Text("DELETE"), ))),
+                  SizedBox(
+                      height: buttonSize,
+                      child: FittedBox(
+                          child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            Uri uri = Uri.parse(_urlController.text);
+                            _future = http.post(uri);
+                          });
+                        },
+                        child: const Text("POST"),
+                      ))),
+                  SizedBox(
+                      height: buttonSize,
+                      child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                Uri uri = Uri.parse(_urlController.text);
+                                _future = http.get(uri);
+                              });
+                            },
+                        child: const Text("GET"),
+                      ))),
+                  SizedBox(
+                      height: buttonSize,
+                      child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                Uri uri = Uri.parse(_urlController.text);
+                                _future = http.put(uri);
+                              });
+                            },
+                        child: const Text("PUT"),
+                      ))),
+                  SizedBox(
+                      height: buttonSize,
+                      child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                Uri uri = Uri.parse(_urlController.text);
+                                _future = http.patch(uri);
+                              });
+                            },
+                        child: const Text("PATCH"),
+                      ))),
+                  SizedBox(
+                      height: buttonSize,
+                      child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                Uri uri = Uri.parse(_urlController.text);
+                                _future = http.delete(uri);
+                              });
+                            },
+                        child: const Text("DELETE"),
+                      ))),
                 ],
               ),
             ),
-            Expanded(child: Padding(
+            Expanded(
+                child: Padding(
               padding: const EdgeInsets.all(32.0),
-              child: Container(color: Colors.black12, child: TextField(readOnly: true, keyboardType: TextInputType.multiline, maxLines: 1000)),
+              child: Stack(
+                children: [
+                  if (_future != null)
+                    FutureBuilder(
+                      future: _future,
+                      builder: (context, snapshot) {
+                        print("snapshot update $snapshot");
+                        if (snapshot.hasError) {
+                          return Container(
+                              alignment: Alignment.center,
+                              child: SizedBox(
+                                  width: 60,
+                                  height: 60,
+                                  child: FittedBox(child: Icon(Icons.error_outline_rounded, color: Theme.of(context).primaryColor,))));
+                        } else if (snapshot.hasData) {
+                          WidgetsBinding.instance
+                              .addPostFrameCallback((timeStamp) {
+                            _responseController.text = snapshot.data!.body;
+                          });
+                          return Container();
+                        } else {
+                          return Container(
+                              alignment: Alignment.center,
+                              child: const SizedBox(
+                                width: 60,
+                                height: 60,
+                                child: CircularProgressIndicator(),
+                              ));
+                        }
+                      },
+                    ),
+                  Container(
+                      color: Colors.black12,
+                      child: TextField(
+                          controller: _responseController,
+                          readOnly: true,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 1000))
+                ],
+              ),
             ))
             //Expanded(child: Placeholder())
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Send Request',
-        child: const Icon(Icons.send),
+        onPressed: () {
+          setState(() {
+            _future = null;
+            _responseController.text = "";
+          });
+        },
+        tooltip: 'Clear',
+        child: const Icon(Icons.delete_forever),
       ),
     );
   }
 }
-/*
-class RadioElevatedButtonRow extends StatefulWidget {
-  final List<String> options;
-  final int selected;
-  final Function(int) onSelectionChange;
-
-  const RadioElevatedButtonRow({Key? key, required this.options, required this.selected, required this.onSelectionChange}) : super(key: key);
-
-  @override
-  State<RadioElevatedButtonRow> createState() => _RadioElevatedButtonRowState();
-}
-
-class _RadioElevatedButtonRowState extends State<RadioElevatedButtonRow> {
-  @override
-  Widget build(BuildContext context) {
-    return
-  }
-}*/
